147d1e8b575191ec0b0f11d113043a54402c83f3
Eric <naibau2004@gamil.com>
2014-12-16 23:13:35 +0800

    Develop 20141216

---
+++ src/h20141105_ex6/Ex6.java
@@ -0,0 +1,17 @@
+package h20141105_ex6;
+
+public class Ex6 
+{	
+	public static void main ( String [] args) 
+	{
+	for (int x =1;x<10;x++)
+	{
+		System.out.println(x +"");
+	}
+	}
+}
+
+
+
+
+
---
+++ src/testHomework/test.java
@@ -0,0 +1,15 @@
+package testHomework;
+
+public class test 
+{
+	
+	public static void main ( String [] args )
+	{
+		double f = 1234.4 ;
+		int x = (int)( f + 0.9 ) ;
+		System.out.println ( x ) ;
+		
+		
+		
+	}	
+}
